7033e5fb47cbb3bf4d249f885d648641d65f5be8
Tommy Killian <thomaskillian13@gmail.com>
2024-07-28 13:40:09 -0500

    no map yet

--- script.js
+++ script.js
@@ -1,96 +1,107 @@
+let currentScene = 0;
+let data;
+
 Promise.all([
     d3.csv("goal15.forest_shares.csv"),
     d3.json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json")
-]).then(function([data, world]) {
-    data.forEach(d => {
-        d.forest_2000 = +d.forest_2000;
-        d.forest_2020 = +d.forest_2020;
-        d.trend = +d.trend;
-    });
-
-    const declines = getDeclines(data)
-    const growths = getGrowths(data)
-
-    createBarChart("#visualization", declines, "Declines in Forestation");
-    createBarChart("#visualization", growths, "Growths in Forestation");
-
-    createMap("#map", data, world);
-}).catch(error => {
-    console.error("Error loading the data:", error);
+]).then(function([deforestationData, world]) {
+    data = deforestationData; 
+    initScenes(data, world);
 });
+
+function initScenes(data, world) {
+    renderScene(currentScene, data, world);
+
+    d3.select("#next").on("click", () => {
+        currentScene++;
+        if (currentScene < 3) {
+            renderScene(currentScene, data, world);
+        }
+    });
+}
+
+function renderScene(scene, data, world) {
+    d3.select("#visualization").html(""); 
+    switch (scene) {
+        case 0:
+            showGlobalStats(data);
+            break;
+        case 1:
+            createBarChart("#visualization", getDeclines(data), "Declines in Forestation");
+            break;
+        case 2:
+            createBarChart("#visualization", getGrowths(data), "Growths in Forestation");
+            break;
+        default:
+            // createMap("#map", data, world);
+            break;
+    }
+}
 
 function getDeclines(data) {
     return data
-        .filter(d => !isNaN(d.trend))
+        .filter(d => !isNaN(d.trend)) 
         .sort((a, b) => a.trend - b.trend)
         .slice(0, 5);
 }
 
 function getGrowths(data) {
     return data
-        .filter(d => !isNaN(d.trend))
+        .filter(d => !isNaN(d.trend)) 
         .sort((a, b) => b.trend - a.trend)
         .slice(0, 5);
 }
 
-
 function createBarChart(selector, data, title) {
-    const svg = d3.select(selector).append("svg")
-        .attr("width", 800)
-        .attr("height", 400 + 50);
+    d3.select(selector).html("");
 
-    const xScale = d3.scaleBand()
+    const margin = {top: 20, right: 30, bottom: 40, left: 70};
+    const width = 600 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
+
+    const svg = d3.select(selector).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    const x = d3.scaleBand()
         .domain(data.map(d => d.iso3c))
-        .range([0, 600])
+        .range([0, width])
         .padding(0.1);
-    
-    const yScale = d3.scaleLinear()
+
+    const y = d3.scaleLinear()
         .domain([d3.min(data, d => d.trend), d3.max(data, d => d.trend)])
-        .range([400, 0]);
-    
+        .nice()
+        .range([height, 0]);
+
     svg.append("g")
-        .attr("transform", "translate(0,400)")
-        .call(d3.axisBottom(xScale));
-    
+        .attr("class", "x-axis")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(x));
+
     svg.append("g")
-        .call(d3.axisLeft(yScale));
+        .attr("class", "y-axis")
+        .call(d3.axisLeft(y));
 
     svg.selectAll(".bar")
         .data(data)
         .enter().append("rect")
         .attr("class", "bar")
-        .attr("x", d => xScale(d.iso3c))
-        .attr("y", d => yScale(d.trend))
-        .attr("width", xScale.bandwidth())
-        .attr("height", d => 400 - yScale(d.trend))
+        .attr("x", d => x(d.iso3c))
+        .attr("y", d => y(d.trend))
+        .attr("width", x.bandwidth())
+        .attr("height", d => height - y(d.trend))
         .attr("fill", d => d.trend < 0 ? "red" : "green");
-    
-    svg.selectAll(".label")
+
+    svg.selectAll(".value-label")
         .data(data)
         .enter().append("text")
-        .attr("class", "label")
-        .attr("x", d => xScale(d.iso3c) + xScale.bandwidth() / 2)
-        .attr("y", 420)
-        .attr("text-anchor", "middle")
-        .text(d => d.iso3c);
+        .attr("class", "value-label")
+        .attr("x", d => x(d.iso3c) - 10)
+        .attr("y", d => y(d.trend) + 4)
+        .text(d => d.trend) 
+        .style("font-size", "12px")
+        .style("fill", "black")
+        .style("text-anchor", "end"); 
 }
-
-// function createMap(selector, data, world) {
-//     const svg = d3.select(selector).append("svg")
-//         .attr("width", 800)
-//         .attr("height", 400);
-
-//     const projection = d3.geoMercator().fitSize([800, 400], world);
-//     const path = d3.geoPath().projection(projection);
-
-//     svg.selectAll("path")
-//         .data(topojson.feature(world, world.objects.countries).features)
-//         .enter().append("path")
-//         .attr("d", path)
-//         .attr("fill", function(d) {
-//             const country = data.find(c => c.iso3c === d.id);
-//             return country ? (country.trend < 0 ? "red" : "green") : "lightgrey";
-//         })
-//         .attr("stroke", "white")
-//         .attr("stroke-width", 0.5);
-// }
