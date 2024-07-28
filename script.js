987414c546ae34b7ed2a2aa08412bea66f0b90da
Tommy Killian <thomaskillian13@gmail.com>
2024-07-28 11:44:51 -0500

    five scenes

--- script.js
+++ script.js
@@ -34,12 +34,26 @@ function renderScene(sceneIndex) {
     .attr('cy', 300)
     .attr('r', 150)
     .style('fill', 'yellow');
-  }
+  }  else if (sceneIndex === 3) {
+    // Scene 2: Example content
+    svg.append('rect')
+      .attr('x', 350)
+      .attr('y', 250)
+      .attr('width', 300)
+      .attr('height', 500)
+      .style('fill', 'orange');
+  } else if (sceneIndex === 4) {
+    svg.append('circle')
+    .attr('cx', 400)
+    .attr('cy', 300)
+    .attr('r', 75)
+    .style('fill', 'red');
+  } 
   // Add more scenes as needed
 }
 
 d3.select('#nextButton').on('click', function() {
-  currentScene = (currentScene + 1) % 3;  // Cycle through scenes
+  currentScene = (currentScene + 1) % 5;  // Cycle through scenes
   renderScene(currentScene);
 });
 
